8dce37c8c38743f5583ec6b9cacd039dd2d78242
Jeff Kassel <jkassel@gmail.com>
2018-05-26 12:05:13 -0400

    Adding Projects Page

--- server.js
+++ server.js
@@ -60,6 +60,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'This is the projects page'
+    });
+});
+
 app.get('/bad', (req, res) => {
    res.send({
        error: 'error handling request'
